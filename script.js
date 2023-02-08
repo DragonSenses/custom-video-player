f0cfa5c49c37cfe2c64e261060ce405404b62ecf
DragonSenses <kendrickfrancisco@gmail.com>
2023-02-08 13:36:08 -0800

    Create event handlers & variables

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-/* in index.html
-    <video
-      src="videos/rabbit320.mp4"
-      id="video"
-      class="screen"
-      poster="img/poster.png"
-    ></video>
-    <div class="controls">
-      <button class="btn" id="play">
-        <!-- fontawesome icons -->
-        <i class="fa fa-play fa-2x"></i>
-      </button>
-      <button class="btn" id="stop">
-        <i class="fa fa-stop fa-2x"></i>
-      </button>
-      <input type="range" id="progress" class="progress" min="0" max="100" step="0.1"
-        value="0">
-      <span class="timestamp" id="timestamp">00:00</span>
-      
-    </div>
-    
-*/
+/* Global Variables */
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timeStamp = document.getElementById('timeStamp');
+
+// Play & Pause video
+function toggleVideoStatus(){
+  /* Since video is connected to HTML5 <video> tag, can use API that has
+  methods such as paused() */
+  (video.paused) ? video.play() : video.pause();
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+  if(video.paused) {
+    play.innerHtml = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+      <path fill-rule="evenodd" d="M6.75 5.25a.75.75 0 01.75-.75H9a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H7.5a.75.75 0 01-.75-.75V5.25zm7.5 0A.75.75 0 0115 4.5h1.5a.75.75 0 01.75.75v13.5a.75.75 0 01-.75.75H15a.75.75 0 01-.75-.75V5.25z" clip-rule="evenodd" />
+    </svg>`;
+    console.log(play);
+    console.log(play.innerHTML);
+  } else {
+    play.innerHtml = `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+      <path fill-rule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clip-rule="evenodd" />
+    </svg> `;
+    console.log(play);
+    console.log(play.innerHTML);
+
+  }
+}
+ 
+// Update progress & timestamp
+function updateProgress(){
+  return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+  return true;
+
+}
+
+// Stops & Resets the video progress
+function stopVideo(){
+  // Reset current time to 0 and pauses it
+  video.currentTime = 0;
+  video.pause();
+}
+
+
+/* Event Listeners */
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+play.addEventListener('click', toggleVideoStatus);
+stop.addEventListener('click', stopVideo);
+
+progress.addEventListener('change', setVideoProgress);
